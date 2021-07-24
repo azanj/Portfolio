646bc16e206d037234de0d02148190f7d0d046c6
azanj <alizanjani@bigpond.com>
2021-07-24 13:38:34 +0000

    Input password and confirm it is the same as the hash password stored

---
+++ scripts/portfolio/week2/passwordcheck.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Input password and confirm it is the same as the hash password stored 
+
+read -sp 'Enter secret Password' var_spass
+var_hash=$(echo $var_spass | sha256sum)
+
+hashfile="secret.txt"
+myvariable=$(cat "$hashfile")
+
+echo "$var_hash"
+echo "$myvariable"
+
+
+if [ "$var_hash" == "$myvariable" ];then
+echo "Access Granted"
+exit 0 
+
+else 
+
+echo "Access Denied" 
+exit 1 
+fi
