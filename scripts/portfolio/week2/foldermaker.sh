8d432268c6a3baa71f45e2e37f107e6008426aa9
azanj <alizanjani@bigpond.com>
2021-07-25 12:18:47 +0000

    Foldermaker script

---
+++ scripts/portfolio/week2/foldermaker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Ali Zanjani 25/7/21 This script will automaticlly create a new folder
+read -p "type the name of the folder you would like to create" folderName
+
+Mkdir "$folderName"
